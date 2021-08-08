5b209b4504ca9d17ef58872ae69029a5ba88d12d
Lawrence Hunt <lawrence.hunt@gmail.com>
2021-08-08 17:33:17 +1000

    chore: Update device id and remove tf

--- tf/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "iot_homebridge_access_id" {
-  value = aws_iam_access_key.iot_homebridge.id
-}
-
-output "iot_homebridge_access_secret" {
-  value = aws_iam_access_key.iot_homebridge.secret
-}
